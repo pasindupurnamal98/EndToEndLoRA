f42e4f8f0eff7ed475b25ae1b74d4bd6ffa095bc
pasindupurnamal98 <sasindunisal@gmail.com>
2025-08-20 09:41:06 +0530

    create syntheticdatagentest.py

---
+++ syntheticdatagentest.py
@@ -0,0 +1,15 @@
+from docling.document_converter import DocumentConverter
+from docling.chunking import HybridChunker
+from colorama import Fore 
+
+if __name__ == "__main__": 
+    converter = DocumentConverter()
+    doc = converter.convert("tm1_dg_dvlpr-10pages.pdf").document
+    chunker = HybridChunker()
+    chunks = chunker.chunk(dl_doc=doc)
+
+    dataset = {}
+    for i, chunk in enumerate(chunks): 
+            print(Fore.YELLOW + f"Raw Text:\n{chunk.text[:300]}…" + Fore.RESET)
+            enriched_text = chunker.contextualize(chunk=chunk)
+            print(Fore.LIGHTMAGENTA_EX + f"Contextualized Tex:\n{enriched_text[:300]}…" + Fore.RESET)
